3fe5690a11693c7c4f72c3033c8da125996c8a54
liil444 <lr_5146@163.com>
2012-11-10 10:32:55 +0800

    bbb

--- HelloWorld/HelloWorldLayer.m
+++ HelloWorld/HelloWorldLayer.m
@@ -49,7 +49,7 @@
 		CGSize size = [[CCDirector sharedDirector] winSize];
 	
 		// position the label on the center of the screen
-		label.position =  ccp( size.width /2 , size.height/2 );
+		label.position =  ccp( size.width /3 , size.height/2 );
 		
 		// add the label as a child to this Layer
 		[self addChild: label];
